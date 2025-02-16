b804f40c015fcc997b3c27336081cdc1f3d5c559
YuvalFireFly <yuval@firefly.ai>
2025-02-16 17:40:48 +0200

    Update Terraform configuration for AWS demo project
    - Change subnet from data source to resource
    - Modify subnet CIDR block
    - Remove hardcoded key name
    - Add tags to subnet
    - Update .gitignore to exclude Terraform local files

--- terraform-aws-demo/main.tf
+++ terraform-aws-demo/main.tf
@@ -32,10 +32,14 @@ resource "aws_security_group" "instance_sg" {
   }
 }
 
-data "aws_subnet" "main" {
+resource "aws_subnet" "main" {
   vpc_id            = data.aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
   availability_zone = "${var.aws_region}a"
-  default_for_az    = true
+
+  tags = {
+    Name = "${var.project_name}-subnet"
+  }
 }
 
 module "ec2_instance" {
@@ -44,10 +48,9 @@ module "ec2_instance" {
   name = "main-instance"
 
   instance_type          = "t2.micro"
-  key_name               = "main-key"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.instance_sg.id]
-  subnet_id              = data.aws_subnet.main.id
+  subnet_id              = aws_subnet.main.id
 
   tags = {
     Name = "${var.project_name}-instance"
--- terraform-aws-demo/variables.tf
+++ terraform-aws-demo/variables.tf
@@ -10,16 +10,10 @@ variable "project_name" {
   default     = "demo"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
 variable "subnet_cidr" {
   description = "CIDR block for subnet"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "172.25.0.0/16"
 }
 
 variable "instance_ami" {
