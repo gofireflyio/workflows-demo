3ac783bc2b677d854f2018a59a55196d4d452d56
YuvalFireFly <yuval@firefly.ai>
2025-02-16 16:43:20 +0200

    Update EC2 instance subnet configuration to use data source

--- terraform-aws-demo/main.tf
+++ terraform-aws-demo/main.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "main" {
   ami           = var.instance_ami
   instance_type = "t2.micro"
 
-  subnet_id                   = aws_subnet.main.id
+  subnet_id                   = data.aws_subnet.main.id
   vpc_security_group_ids     = [aws_security_group.instance_sg.id]
   associate_public_ip_address = true
 
