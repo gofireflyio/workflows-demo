5b50d6276f5289774409fb28b0bebb20ab87c1d8
glengol <152513160+glengol@users.noreply.github.com>
2026-05-07 18:08:45 +0300

    [Firefly]: context aware codification for aws_instance

--- aws-env/outputs.tf
+++ aws-env/outputs.tf
@@ -6,4 +6,19 @@ output "instance_id" {
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
   value       = aws_s3_bucket.data.id
+}
+
+output "gal_test2_instance_id" {
+  description = "ID of the gal-test2 EC2 instance"
+  value       = module.gal_test2_ec2_instance.id
+}
+
+output "gal_test2_instance_public_ip" {
+  description = "Public IP of the gal-test2 EC2 instance"
+  value       = module.gal_test2_ec2_instance.public_ip
+}
+
+output "gal_test2_instance_private_ip" {
+  description = "Private IP of the gal-test2 EC2 instance"
+  value       = module.gal_test2_ec2_instance.private_ip
 } 
